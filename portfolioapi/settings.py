1c6bdc5b38c637b4943d30351a8c94b774fd995e
SrinithishB <srinithishofficial@gmail.com>
2024-02-03 10:39:17 +0530

    'mysql2'

--- portfolioapi/settings.py
+++ portfolioapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h18!&ae7@b)sn=!aq^7bp$+ell7cx7$odjeum95(&p!5@11=!q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -68,17 +68,29 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolioapi.wsgi.application'
+# WSGI_APPLICATION = 'portfolioapi.wsgi.application'
+WSGI_APPLICATION = 'portfolioapi.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'portfolio',
+		'USER': 'root',
+		'PASSWORD': 'password',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
